9d6bfe56bfda8333132fd38fe8a0b60dde8adaba
Evan Pratten <ewpratten@gmail.com>
2021-01-03 13:48:09 -0500

    move the wrapper to its own subproject

--- run-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-javac -cp build:javacpp.jar  -sourcepath src src/com/raylib/Test.java -d build
-java -cp build:javacpp.jar  com/raylib/Test -Djava.library.path="build"  com/raylib/Test
-
-#-XstartOnFirstThread com/raylib/Test
