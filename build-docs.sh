212fe1203d705f8571d90bcdb4e12539fa75704e
richard <github@fantastic.me.uk>
2021-07-05 16:47:44 +0100

    build fix

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf doc
-javadoc -d doc -sourcepath gen:src com.raylib -classpath javacpp.jar
+javadoc -d doc -sourcepath gen:src com.raylib -classpath javacpp.jar:jaylib-$RAYLIB_VERSION.jar
 jar cf jaylib-docs-$RAYLIB_VERSION.jar -C doc .
--- build-native.sh
+++ build-native.sh
@@ -2,7 +2,7 @@
 echo "STEP 1 - move compilation results from gen folder to natives folder"
 rm -rf natives
 mkdir -p natives/com/raylib/$RAYLIB_PLATFORM
-mv -f gen/com/raylib/$PLATFORM/* natives/com/raylib/$RAYLIB_PLATFORM/
+mv -f gen/com/raylib/$RAYLIB_PLATFORM/* natives/com/raylib/$RAYLIB_PLATFORM/
 
 echo "STEP 2 -  native jar archive"
 jar cf jaylib-natives-$RAYLIB_PLATFORM-$RAYLIB_VERSION.jar -C natives .
