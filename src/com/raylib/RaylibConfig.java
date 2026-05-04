713c092891781fcbfabaf034e38f590f22c8b946
Richard Smith <github@electronstudio.co.uk>
2026-05-04 05:46:40 +0100

    Update RaylibConfig.java

--- src/com/raylib/RaylibConfig.java
+++ src/com/raylib/RaylibConfig.java
@@ -48,7 +48,7 @@ import org.bytedeco.javacpp.tools.InfoMapper;
         @Platform(
             value = { "linux-x86_64" },
             linkpath = { "/usr/lib/x86_64-linux-gnu" },
-            link = { "X11", "raylib", "SDL3" }
+            link = { "X11", "SDL3", "raylib" }
         ),
         @Platform(
             value = { "linux-arm64" },
