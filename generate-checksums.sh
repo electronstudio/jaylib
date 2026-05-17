1da9a1a18a263940a7b3d0a19a1e33797c21ec6a
Richard Smith <github@electronstudio.co.uk>
2026-05-17 19:15:19 +0100

    fix checksums for sonotype

--- generate-checksums.sh
+++ generate-checksums.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=6.0.1-0
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 jar xf jaylib-natives-linux-arm64-${VERSION}.jar
 rm jaylib-natives-linux-arm64-${VERSION}.jar
@@ -20,15 +21,22 @@ rm jaylib-natives-windows-x86_64-${VERSION}.jar
 jar uf jaylib-${VERSION}.jar com
 
 rm -rf com META-INF
-cp ~/IdeaProjects/jaylib/jaylib-${VERSION}.pom .
 
-for FILE in *.jar *.pom
+
+OUT_DIR="uk/co/electronstudio/jaylib/jaylib/${VERSION}"
+mkdir -p ${OUT_DIR}
+
+cp "${SCRIPT_DIR}/jaylib-${VERSION}.pom" "${OUT_DIR}"
+mv *.jar ${OUT_DIR}
+
+for FILE in "${OUT_DIR}"/*
 do
-    md5sum $FILE > $FILE.md5
-    sha1sum $FILE > $FILE.sha1
-    sha512sum $FILE > $FILE.sha512
-    sha256sum $FILE > $FILE.sha256
+    echo "processing ${FILE}"
+    md5sum "${FILE}" | cut -d ' ' -f 1 > "${FILE}".md5
+    sha1sum "${FILE}" | cut -d ' ' -f 1 > "${FILE}".sha1
+    sha512sum "${FILE}" | cut -d ' ' -f 1 > "${FILE}".sha512
+    sha256sum "${FILE}" | cut -d ' ' -f 1 > "${FILE}".sha256
     gpg -ab $FILE
 done
 
-jar -cvf bundle.jar *
+zip -r bundle uk
